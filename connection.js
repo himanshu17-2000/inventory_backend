cca13160f1d878f626f9eb5028bc14f42b2350d1
himanshu17-2000 <64321714+himanshu17-2000@users.noreply.github.com>
2022-10-07 19:35:36 +0530

    initial steps prostgres k liye

--- connection.js
+++ connection.js
@@ -1,8 +1,21 @@
-import pg from 'pg' 
-export const client = new pg.Client({
-    host :"localhost" ,
-    user :"postgres" ,
-    port :5432 ,
-    password :"Himanshu@7024" ,
-    database:"Himanshu_first_db"
-})
+// import pg from 'pg' 
+// export const client = new pg.Client({
+//     host :"localhost" ,
+//     user :"postgres" ,
+//     port :5432 ,
+//     password :"Himanshu@7024" ,
+//     database:"Himanshu_first_db"
+// })
+// const Pool = require("pg").Pool;
+import pg from 'pg'
+const Pool = pg.Pool 
+require("dotenv").config();
+
+const isProduction = process.env.NODE_ENV === "production";
+const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+export const pool = new Pool({
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    // ssl: {
+    //     rejectUnauthorized: false,
+    // },
+});
